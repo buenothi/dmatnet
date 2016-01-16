d581ec06b91c11f737257d820bf1c040c4768496
buenothi <bueno_thiago@outlook.com>
2016-01-16 20:10:02 -0200

    teste

---
+++ src/main/java/br/com/smartems/dmatnet/EJB/dao/teste.java
@@ -0,0 +1,20 @@
+package br.com.smartems.dmatnet.EJB.dao;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class teste
+ */
+@Stateless
+@LocalBean
+public class teste {
+
+    /**
+     * Default constructor. 
+     */
+    public teste() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
