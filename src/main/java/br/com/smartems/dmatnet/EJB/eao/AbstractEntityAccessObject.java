2b676ce3e91233b784ed1a3a09ff09a0f0876b5e
buenothi <bueno_thiago@outlook.com>
2016-01-16 12:46:01 -0200

    Commit inicial do dia - Criação do AbstractEAO - DAO Pattern

---
+++ src/main/java/br/com/smartems/dmatnet/EJB/eao/AbstractEntityAccessObject.java
@@ -0,0 +1,20 @@
+package br.com.smartems.dmatnet.EJB.eao;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class EntityAccessObject
+ */
+@Stateless
+@LocalBean
+public class AbstractEntityAccessObject {
+
+    /**
+     * Default constructor. 
+     */
+    public AbstractEntityAccessObject() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
