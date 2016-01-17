3b07a35fb770b93f51dd71b46862db93328f0866
buenothi <bueno_thiago@outlook.com>
2016-01-17 00:59:09 -0200

    Signed-off-by: buenothi <bueno_thiago@outlook.com>

--- src/main/java/br/com/smartems/dmatnet/EJB/dao/AbstractEAO.java
+++ src/main/java/br/com/smartems/dmatnet/EJB/dao/AbstractEAO.java
@@ -7,6 +7,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 /**
  * Session Bean implementation class EntityAccessObject
@@ -16,37 +17,37 @@ import javax.persistence.PersistenceContext;
 @SuppressWarnings("unchecked")
 public class AbstractEAO <T, PK> {
 
-//	
-//	private EntityManager entityManager;
-//
-//	public AbstractEAO() {
-//		
-//	}
-//
-//	public T read(PK pk) {
-//		return (T) entityManager.find(getTypeClass(), pk);
-//	}
-//
-//	public void create(T entity) {
-//		entityManager.persist(entity);
-//	}
-//
-//	public T update(T entity) {
-//		return this.entityManager.merge(entity);
-//	}
-//
-//	public void delete(T entity) {
-//		entityManager.remove(entity);
-//	}
-//
-//	public List<T> findAll() {
-//		return entityManager.createQuery(("FROM " + getTypeClass().getName())).getResultList();
-//	}
-//
-//	private Class<?> getTypeClass() {
-//		Class<?> clazz = (Class<?>) ((ParameterizedType) this.getClass().getGenericSuperclass())
-//				.getActualTypeArguments()[1];
-//		return clazz;
-//	}
+	@PersistenceContext(unitName = "dmatnet-pu")
+	private EntityManager entityManager;
+
+	public AbstractEAO() {
+		
+	}
+
+	public T read(PK pk) {
+		return (T) entityManager.find(getTypeClass(), pk);
+	}
+
+	public void create(T entity) {
+		entityManager.persist(entity);
+	}
+
+	public T update(T entity) {
+		return this.entityManager.merge(entity);
+	}
+
+	public void delete(T entity) {
+		entityManager.remove(entity);
+	}
+
+	public List<T> findAll() {
+		return entityManager.createQuery(("FROM " + getTypeClass().getName())).getResultList();
+	}
+
+	private Class<?> getTypeClass() {
+		Class<?> clazz = (Class<?>) ((ParameterizedType) this.getClass().getGenericSuperclass())
+				.getActualTypeArguments()[1];
+		return clazz;
+	}
 
 }
