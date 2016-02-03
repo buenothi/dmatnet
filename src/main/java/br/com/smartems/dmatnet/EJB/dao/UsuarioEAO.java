7d80a1031918fe32fca5f01f880fd4888d1f97dd
buenothi <bueno_thiago@outlook.com>
2016-02-03 19:06:28 -0200

    Finalização da Classe UsuarioEAO

--- src/main/java/br/com/smartems/dmatnet/EJB/dao/UsuarioEAO.java
+++ src/main/java/br/com/smartems/dmatnet/EJB/dao/UsuarioEAO.java
@@ -13,17 +13,36 @@ public class UsuarioEAO extends AbstractEAO<UsuarioEntity, Long> implements Usua
     
 	@PersistenceContext
     private EntityManager em;
+	private UsuarioEntity usuario;
+	private List<UsuarioEntity> usuarios;
 	
     public UsuarioEAO() {
         super();
     }
     
     public UsuarioEntity logarUsuario(String login, String senha) {
-    	return null;
+    	this.usuario = (UsuarioEntity)em.createNamedQuery("Usuario.logarUsuario")
+    			.setParameter("login", login)
+    			.setParameter("senha", senha)
+    			.getSingleResult();
+    	return usuario;
     }
     
-    public List<UsuarioEntity> listarUsuariosFilhos(UsuarioEntity usuario){
-    	return null;
+    @SuppressWarnings("unchecked")
+	public List<UsuarioEntity> listarUsuariosFilhos(UsuarioEntity usuario){
+    	this.usuarios = (List<UsuarioEntity>)em.createNamedQuery("Usuario.listarUsuariosFilhos")
+    			.setParameter("idUsuarioPai", usuario.getIdUsuarioPai())
+    			.getResultList();
+    	return usuarios;
     }
 
+    public String gerarNovaSenha(UsuarioEntity usuario){
+    	String[] carct ={"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        String senha="";
+        for (int x=0; x<10; x++){
+            int j = (int) (Math.random()*carct.length);
+            senha += carct[j];
+            }
+        return senha;
+    }
 }
--- src/main/java/br/com/smartems/dmatnet/entities/pessoa/PessoaFisica/Usuario/UsuarioEntity.java
+++ src/main/java/br/com/smartems/dmatnet/entities/pessoa/PessoaFisica/Usuario/UsuarioEntity.java
@@ -1,13 +1,29 @@
 package br.com.smartems.dmatnet.entities.pessoa.PessoaFisica.Usuario;
 
-import br.com.smartems.dmatnet.entities.pessoa.PessoaFisica.AbstractPessoaFisicaEntity;
 import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import br.com.smartems.dmatnet.entities.pessoa.PessoaFisica.AbstractPessoaFisicaEntity;
 
 @Entity
 @Table(name="tbl_usuarios")
+@NamedQueries({
+	@NamedQuery(name="Usuario.logarUsuario", 
+			query="SELECT u FROM UsuarioEntity u WHERE u.login=:login AND u.senha=:senha"),
+	@NamedQuery(name="Usuario.listarUsuariosFilhos", 
+			query="SELECT u FROM UsuarioEntity u WHERE u.idUsuarioPai=:idUsuarioPai")
+})
 public class UsuarioEntity extends AbstractPessoaFisicaEntity implements Serializable {
 
 	@Column(unique = true)
@@ -20,6 +36,7 @@ public class UsuarioEntity extends AbstractPessoaFisicaEntity implements Seriali
 		joinColumns=@JoinColumn(name="usuario_ID"),
 		inverseJoinColumns=@JoinColumn(name="grupo_ID"))
 	private List<UsuariosGrupoEntity> grupos;
+	private long idUsuarioPai;
 
 	private static final long serialVersionUID = 1L;
 
@@ -40,7 +57,20 @@ public class UsuarioEntity extends AbstractPessoaFisicaEntity implements Seriali
 	}
 
 	public void setSenha(String senha) {
-		this.senha = senha;
+		
+		MessageDigest algoritmo;
+		try {
+			algoritmo = MessageDigest.getInstance("SHA-256");
+			byte messageDigest[] = algoritmo.digest(senha.getBytes());
+			StringBuilder hexString = new StringBuilder();
+			for(byte b : messageDigest) {
+				hexString.append(String.format("%02X", 0xFF & b));
+			}			
+			this.senha = hexString.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	
 	}
 
 	public int getUsuarioTipo() {
@@ -51,12 +81,20 @@ public class UsuarioEntity extends AbstractPessoaFisicaEntity implements Seriali
 		this.usuarioTipo = usuarioTipo;
 	}
 
-	public List<UsuariosGrupoEntity> getGrupo() {
+	public List<UsuariosGrupoEntity> getGrupos() {
 		return grupos;
 	}
 
-	public void setGrupo(List<UsuariosGrupoEntity> grupos) {
+	public void setGrupos(List<UsuariosGrupoEntity> grupos) {
 		this.grupos = grupos;
 	}
 
+	public long getIdUsuarioPai() {
+		return idUsuarioPai;
+	}
+
+	public void setIdUsuarioPai(long idUsuarioPai) {
+		this.idUsuarioPai = idUsuarioPai;
+	}
+		
 }
