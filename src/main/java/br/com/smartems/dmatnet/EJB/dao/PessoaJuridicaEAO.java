416a683b195b16c0a3bd0a960a3c3e07f9adeb20
buenothi <bueno_thiago@outlook.com>
2016-03-23 17:32:59 -0300

    Criação de PessoaJuridicaEAO

---
+++ src/main/java/br/com/smartems/dmatnet/EJB/dao/PessoaJuridicaEAO.java
@@ -0,0 +1,17 @@
+package br.com.smartems.dmatnet.EJB.dao;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+import br.com.smartems.dmatnet.entities.pessoa.PessoaJuridica.AbstractPessoaJuridicaEntity;
+
+@Stateless
+@Local
+public class PessoaJuridicaEAO extends AbstractEAO<AbstractPessoaJuridicaEntity, Long> {
+
+    public PessoaJuridicaEAO() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
--- src/main/java/br/com/smartems/dmatnet/EJB/dao/TrabalhadorEAO.java
+++ src/main/java/br/com/smartems/dmatnet/EJB/dao/TrabalhadorEAO.java
@@ -2,20 +2,16 @@ package br.com.smartems.dmatnet.EJB.dao;
 
 import javax.ejb.Local;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import br.com.smartems.dmatnet.entities.pessoa.PessoaFisica.Trabalhador.TrabalhadorEntity;
 
 @Stateless
 @Local
 public class TrabalhadorEAO extends AbstractEAO<TrabalhadorEntity, Long> {
-    
-	@PersistenceContext
-    private EntityManager em;
-	
+       
     public TrabalhadorEAO() {
         super();
+        // TODO Auto-generated constructor stub
     }
-    
+
 }
