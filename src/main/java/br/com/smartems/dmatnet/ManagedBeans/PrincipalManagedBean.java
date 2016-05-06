ed6a3ac8bd5352f3609923171fc3e0f26fb39daa
buenothi <bueno_thiago@outlook.com>
2016-05-06 12:45:16 -0300

    Correção do link home no menu principal

---
+++ src/main/java/br/com/smartems/dmatnet/ManagedBeans/PrincipalManagedBean.java
@@ -0,0 +1,14 @@
+package br.com.smartems.dmatnet.ManagedBeans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class PrincipalManagedBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
