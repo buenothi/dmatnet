dc1efdbd098283ea8e39908d554411a4bf93682a
buenothi <bueno_thiago@outlook.com>
2016-05-07 15:41:15 -0300

    Alteração do principal para

--- src/main/java/br/com/smartems/dmatnet/ManagedBeans/PrincipalManagedBean.java
+++ src/main/java/br/com/smartems/dmatnet/ManagedBeans/PrincipalManagedBean.java
@@ -3,12 +3,21 @@ package br.com.smartems.dmatnet.ManagedBeans;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean
 public class PrincipalManagedBean implements Serializable{
 
+	private boolean isCadastroEmpresa = false;
+	
 	private static final long serialVersionUID = 1L;
+
+	public boolean isCadastroEmpresa() {
+		return isCadastroEmpresa;
+	}
 	
-	
+	public void exibirCadastroEmpresa (ActionEvent evt) {
+		this.isCadastroEmpresa = true;
+	}
 
 }
