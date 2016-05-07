f7592f607dcbe3e4696bac123076bb2790395011
buenothi <bueno_thiago@outlook.com>
2016-05-07 16:11:07 -0300

    finalização do principal (cadEmpresa)

--- src/main/java/br/com/smartems/dmatnet/ManagedBeans/PrincipalManagedBean.java
+++ src/main/java/br/com/smartems/dmatnet/ManagedBeans/PrincipalManagedBean.java
@@ -6,18 +6,36 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.event.ActionEvent;
 
 @ManagedBean
-public class PrincipalManagedBean implements Serializable{
+public class PrincipalManagedBean implements Serializable {
 
 	private boolean isCadastroEmpresa = false;
-	
+	private String classMenuBotaoCadEmpresa = "menuBotao";
+	private String classMenuBotaoHome = "menuBotaoSelecionado";
+
 	private static final long serialVersionUID = 1L;
 
 	public boolean isCadastroEmpresa() {
 		return isCadastroEmpresa;
 	}
-	
-	public void exibirCadastroEmpresa (ActionEvent evt) {
-		this.isCadastroEmpresa = true;
+
+	public String getClassMenuBotaoCadEmpresa() {
+		return classMenuBotaoCadEmpresa;
 	}
 
+	public String getClassMenuBotaoHome() {
+		return classMenuBotaoHome;
+	}
+
+	public void exibirCadastroEmpresa(ActionEvent evt) {
+		this.isCadastroEmpresa = true;
+		this.classMenuBotaoCadEmpresa = "menuBotaoSelecionado";
+		this.classMenuBotaoHome = "menuBotao";
+	}
+
+	public void exibirHome(ActionEvent evt) {
+		this.isCadastroEmpresa = false;
+		this.classMenuBotaoCadEmpresa = "menuBotao";
+		this.classMenuBotaoHome = "menuBotaoSelecionado";
+	}
+	
 }
