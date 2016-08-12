51c632da77fa49d7e73d168c6038d0a4f84dee66
thiago <bueno_thiago@outlook.com>
2016-08-12 14:33:33 -0300

    Finalização da Lista de Estados e Cidades (falta Converter Cidade)

--- src/main/java/br/com/smartems/dmatnet/ManagedBeans/EstadoMB.java
+++ src/main/java/br/com/smartems/dmatnet/ManagedBeans/EstadoMB.java
@@ -12,16 +12,19 @@ import javax.faces.event.AjaxBehaviorEvent;
 import javax.persistence.NoResultException;
 
 import br.com.smartems.dmatnet.EJB.Facade.EstadoFacadeLocal;
+import br.com.smartems.dmatnet.entities.cidades.CidadeEntity;
 import br.com.smartems.dmatnet.entities.cidades.EstadoEntity;
 
 @ManagedBean
 @ViewScoped
 public class EstadoMB implements Serializable {
-	
+
 	@EJB
 	private EstadoFacadeLocal estadoFachada;
-	
+
 	private List<EstadoEntity> estados;
+	private EstadoEntity estadoSelecionado;
+	private List<CidadeEntity> cidadesPorEstado;
 
 	private static final long serialVersionUID = 1L;
 
@@ -33,11 +36,35 @@ public class EstadoMB implements Serializable {
 	}
 
 	public void setEstados(List<EstadoEntity> estados) {
+		if (this.estadoSelecionado == null) {
+			this.estadoSelecionado = new EstadoEntity();
+		}
 		this.estados = estados;
 	}
-	
+
+	public EstadoEntity getEstadoSelecionado() {
+		return estadoSelecionado;
+	}
+
+	public void setEstadoSelecionado(EstadoEntity estadoSelecionado) {
+		this.estadoSelecionado = estadoSelecionado;
+	}
+
+	public List<CidadeEntity> getCidadesPorEstado() {
+		if (this.cidadesPorEstado == null) {
+			this.cidadesPorEstado = new ArrayList<CidadeEntity>();
+		}
+		return cidadesPorEstado;
+	}
+
+	public void setCidadesPorEstado(List<CidadeEntity> cidadesPorEstado) {
+		this.cidadesPorEstado = cidadesPorEstado;
+	}
+
 	public void onSelectionEstado(AjaxBehaviorEvent evt) {
-		System.out.println();
+		this.estadoSelecionado = ((EstadoEntity) evt.getComponent().getAttributes().get("value"));
+		this.cidadesPorEstado = this.estadoFachada.listarCidadesPorEstado(estadoSelecionado);
+		System.out.println(this.cidadesPorEstado.get(4));
 	}
 
 	@PostConstruct
@@ -51,7 +78,7 @@ public class EstadoMB implements Serializable {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
