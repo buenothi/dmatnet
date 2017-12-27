1028ce5b79ed77fba0202dc8df739b47545e2ef2
Thiago Gonçalves Bueno <bueno_thiago@outlook.com>
2017-12-27 00:26:36 -0200

    Revert "CadastroEmpresa Endereco - (Salvar com bug JPA corrigido) parte 5"
    This reverts commit eda60060af6fcd77819337ec942a513d5ccc272a.

--- src/main/java/br/com/smartems/dmatnet/entities/pessoa/AbstractPessoaEntity.java
+++ src/main/java/br/com/smartems/dmatnet/entities/pessoa/AbstractPessoaEntity.java
@@ -5,22 +5,20 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity
-@Table(name = "tbl_Pessoa")
+@MappedSuperclass
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class AbstractPessoaEntity implements Serializable {
 
