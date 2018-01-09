d331c73100bbe89ac31df23fda9ed2d3f0dbbf80
thiago <bueno_thiago@outlook.com>
2018-01-09 20:00:02 -0200

    Cadastro Endereço - Bug Logradouros - parte 5

--- src/main/java/br/com/smartems/dmatnet/Converters/LogradouroConverter.java
+++ src/main/java/br/com/smartems/dmatnet/Converters/LogradouroConverter.java
@@ -5,9 +5,11 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
 
 import br.com.smartems.dmatnet.entities.pessoa.EnderecoTipoEntity;
 
+@FacesConverter("logradouroConverter")
 public class LogradouroConverter implements Converter {
 
 	@Override
