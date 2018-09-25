f8f4dc379b618c3dfb969996d1907eb26c7ffdf3
thiago <bueno_thiago@outlook.com>
2018-09-25 16:57:08 -0300

    Cadastro Empregado - Parte 32 - ligando view no MB

--- src/main/java/br/com/smartems/dmatnet/ManagedBeans/CadastroEmpregadoMB.java
+++ src/main/java/br/com/smartems/dmatnet/ManagedBeans/CadastroEmpregadoMB.java
@@ -1,10 +1,16 @@
 package br.com.smartems.dmatnet.ManagedBeans;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import br.com.smartems.dmatnet.entities.pessoa.PessoaFisica.Trabalhador.TrabalhadorEntity;
 
 @ManagedBean
 @SessionScoped
@@ -15,8 +21,93 @@ public class CadastroEmpregadoMB implements Serializable {
 
 	@ManagedProperty(value = "#{principalMB}")
 	private PrincipalMB principalMB;
-	
-	
+
+	@ManagedProperty(value = "#{cadastroEmpresaMB}")
+	private CadastroEmpresaMB cadastroEmpresaMB;
+
 	private static final long serialVersionUID = 1L;
+
+	private List<TrabalhadorEntity> listaEmpregadosEmpresaSelecionada;
+
+	private String filtroEmpregadosEmpresaSelecionada = "Ativo";
+
+	private String nomeEmpregadoProcurado;
+
+	/* getters e setter */
+
+	public UsuarioMB getUsuarioMB() {
+		return usuarioMB;
+	}
+
+	public void setUsuarioMB(UsuarioMB usuarioMB) {
+		this.usuarioMB = usuarioMB;
+	}
+
+	public PrincipalMB getPrincipalMB() {
+		return principalMB;
+	}
+
+	public void setPrincipalMB(PrincipalMB principalMB) {
+		this.principalMB = principalMB;
+	}
+
+	public CadastroEmpresaMB getCadastroEmpresaMB() {
+		return cadastroEmpresaMB;
+	}
+
+	public void setCadastroEmpresaMB(CadastroEmpresaMB cadastroEmpresaMB) {
+		this.cadastroEmpresaMB = cadastroEmpresaMB;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public List<TrabalhadorEntity> getListaEmpregadosEmpresaSelecionada() {
+		return listaEmpregadosEmpresaSelecionada;
+	}
+
+	public void setListaEmpregadosEmpresaSelecionada(List<TrabalhadorEntity> listaEmpregadosEmpresaSelecionada) {
+		this.listaEmpregadosEmpresaSelecionada = listaEmpregadosEmpresaSelecionada;
+	}
+
+	public String getFiltroEmpregadosEmpresaSelecionada() {
+		return filtroEmpregadosEmpresaSelecionada;
+	}
+
+	public void setFiltroEmpregadosEmpresaSelecionada(String filtroEmpregadosEmpresaSelecionada) {
+		this.filtroEmpregadosEmpresaSelecionada = filtroEmpregadosEmpresaSelecionada;
+	}
+
+	/* Fim dos geters e seters */
+
+	public String getNomeEmpregadoProcurado() {
+		return nomeEmpregadoProcurado;
+	}
+
+	public void setNomeEmpregadoProcurado(String nomeEmpregadoProcurado) {
+		this.nomeEmpregadoProcurado = nomeEmpregadoProcurado;
+	}
+
+	public void mudarTipoFiltroListaEmpregados(AjaxBehaviorEvent evt) {
+
+		// teste abaixo ok
+		System.out.println(this.filtroEmpregadosEmpresaSelecionada.toString());
+	}
+	
+	public void filtrarEmpregados(ActionEvent evt) {
+		
+	}
+	
+	public void removerFiltroEmpresa(ActionEvent e) {
+		this.initEmpregado();
+		this.nomeEmpregadoProcurado = null;
+		this.listaEmpregadosEmpresaSelecionada = null;
+	}
+	
+	@PostConstruct
+	public void initEmpregado() {
+		
+	}
 
 }
