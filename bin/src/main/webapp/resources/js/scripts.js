7c953d9bb0e14ed384d3b135d350453f9b2a8c37
Thiago <bueno_thiago@outlook.com>
2018-09-28 14:17:27 -0300

    Autalização Projeto MESTRAPC-2801

---
+++ bin/src/main/webapp/resources/js/scripts.js
@@ -0,0 +1,45 @@
+PrimeFaces.locales['pt'] = {
+	closeText : 'Fechar',
+	prevText : 'Anterior',
+	nextText : 'Próximo',
+	currentText : 'Hoje',
+	monthNames : [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+			'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
+	monthNamesShort : [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago',
+			'Set', 'Out', 'Nov', 'Dez' ],
+	dayNames : [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta',
+			'Sábado' ],
+	dayNamesShort : [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb' ],
+	dayNamesMin : [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+	weekHeader : 'Semana',
+	firstDay : 0,
+	isRTL : false,
+	showMonthAfterYear : false,
+	yearSuffix : '',
+	timeOnlyTitle : 'Só Horas',
+	timeText : 'Tempo',
+	hourText : 'Hora',
+	minuteText : 'Minuto',
+	secondText : 'Segundo',
+	ampm : false,
+	month : 'Mês',
+	week : 'Semana',
+	day : 'Dia',
+	allDayText : 'Todo o Dia'
+};
+
+function openMenu() {
+	document.getElementById("modulos").className = "conteudoFixo";
+	document.getElementById("lateral").className = "lateralFixo";
+	document.getElementById("formLateral:btnMenu").className = "menuBotaoMenuSelecionado";
+	document.getElementById("formLateral:btnMenu").setAttribute('onclick', 'closeMenu()');
+	document.getElementById("logoMenu").setAttribute('id', 'logoMenuFixo');
+}
+
+function closeMenu() {
+	document.getElementById("modulos").className = "conteudo";
+	document.getElementById("lateral").className = "lateral";
+	document.getElementById("formLateral:btnMenu").className = "menuBotao";
+	document.getElementById("formLateral:btnMenu").setAttribute('onclick', 'openMenu()');
+	document.getElementById("logoMenuFixo").setAttribute('id', 'logoMenu');
+}
