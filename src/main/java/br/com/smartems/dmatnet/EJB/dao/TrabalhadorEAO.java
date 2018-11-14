26d1c1492e41c86c7c610c46a46042e59c73238a
Thiago <bueno_thiago@outlook.com>
2018-11-14 17:02:56 -0200

    Cadastro Empregado - Parte 64 - EAO - SalvarNovoEmpregado

--- src/main/java/br/com/smartems/dmatnet/EJB/dao/TrabalhadorEAO.java
+++ src/main/java/br/com/smartems/dmatnet/EJB/dao/TrabalhadorEAO.java
@@ -8,6 +8,7 @@ import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 import br.com.smartems.dmatnet.entities.ClassificacaoFuncional.ClassificacaoFuncionalEntity;
@@ -42,46 +43,87 @@ public class TrabalhadorEAO extends AbstractEAO<TrabalhadorEntity, Long> {
 			List<EnderecoEntity> enderecosHistorico, EmailEntity emailAtual, List<EmailEntity> emailsHistorico,
 			TelefoneEntity telefonePrincipal, List<TelefoneEntity> telefones,
 			List<ClassificacaoFuncionalEntity> classificacoesFuncionais, DeficienciaFisicaEntity deficienciaFisica) {
-		try {
-			if (trabalhadorNovo.getIdPessoa() == 0) {
 
-				EmpresaEntity novaEmpresaSelecionada = pessoaJuridicaEAO.read(empresaSelecionada.getIdPessoa());
+		if (trabalhadorNovo.getIdPessoa() == 0) {
 
+			try {
 				Set<TrabalhadorCadastroEntity> cadastrosCombinados = new TreeSet<TrabalhadorCadastroEntity>();
 				cadastrosCombinados.addAll(trabalhadorListaCadastroHistorico);
 				cadastrosCombinados.add(trabalhadorCadastroAtual);
 				trabalhadorNovo.setCadastrosTrabalhador(cadastrosCombinados);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
+			try {
 				trabalhadorNovo.setDocumentosPessoais(trabalhadorDocumentos);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
+			try {
 				Set<EnderecoEntity> enderecosCombinados = new TreeSet<EnderecoEntity>();
 				enderecosCombinados.addAll(enderecosHistorico);
 				enderecosCombinados.add(enderecoAtual);
 				trabalhadorNovo.setEnderecos(enderecosCombinados);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
+			try {
 				Set<EmailEntity> emailsCombinados = new TreeSet<EmailEntity>();
 				emailsCombinados.addAll(emailsHistorico);
 				emailsCombinados.add(emailAtual);
 				trabalhadorNovo.setEmails(emailsCombinados);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
+			try {
 				Set<TelefoneEntity> telefonesCombinados = new TreeSet<TelefoneEntity>();
 				telefonesCombinados.addAll(telefones);
 				telefonesCombinados.add(telefonePrincipal);
 				trabalhadorNovo.getTelefones().addAll(telefonesCombinados);
-
-				trabalhadorNovo.getClassificacoesFuncionais().addAll(classificacoesFuncionais);
-
-				trabalhadorNovo.setDeficienciaFisica(deficienciaFisica);
-
-				trabalhadorNovo.setUsuarioCriador(usuario);
-
-				novaEmpresaSelecionada.getTrabalhadores().add(trabalhadorNovo);
-
-				this.pessoaJuridicaEAO.update(novaEmpresaSelecionada);
-
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (Exception excp) {
-			excp.printStackTrace();
+
+			try {
+				trabalhadorNovo.getClassificacoesFuncionais().addAll(classificacoesFuncionais);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				trabalhadorNovo.setDeficienciaFisica(deficienciaFisica);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				trabalhadorNovo.setUsuarioCriador(usuario);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				EmpresaEntity novaEmpresaSelecionada;
+				novaEmpresaSelecionada = pessoaJuridicaEAO.read(empresaSelecionada.getIdPessoa());
+				novaEmpresaSelecionada.getTrabalhadores().add(trabalhadorNovo);
+				this.pessoaJuridicaEAO.update(novaEmpresaSelecionada);
+			} catch (NoResultException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 
