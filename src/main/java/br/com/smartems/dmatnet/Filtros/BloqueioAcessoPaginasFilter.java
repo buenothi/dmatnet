93e840d6d2da6997188fc9f9c1e125dfc89a6566
Thiago <Thiago@qosmio-pc>
2020-06-13 23:35:14 -0300

    remoção do import servletContext

--- src/main/java/br/com/smartems/dmatnet/Filtros/BloqueioAcessoPaginasFilter.java
+++ src/main/java/br/com/smartems/dmatnet/Filtros/BloqueioAcessoPaginasFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
